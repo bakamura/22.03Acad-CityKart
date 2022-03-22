562561c5e357667843eb11741b741bab4385d7aa
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2022-03-22 20:00:09 -0300

    props + new cars + fixing sphere car

--- Assets/TestScene/Scripts/Kart/ControlCarSphere.cs
+++ Assets/TestScene/Scripts/Kart/ControlCarSphere.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class ControlCarSphere : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRB;
+    [SerializeField] private SphereCollider sphereCollider;
     [SerializeField] private float fowardAccleration;
     [SerializeField] private float backAcceleration;
     [SerializeField] private float turningStrength;
     [SerializeField] private Transform[] turningWheels;
     private float currentMovment;
     private bool onGround;
+    [SerializeField] private float maxXVehicleTurn;
+    [SerializeField] private float maxZVehicleTurn;
     [SerializeField] private float gravityForce;
     [SerializeField] private float airDrag;
-    [SerializeField] private float rayCastDistance;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Transform rayCastPoint;
     private float baseDragValue;
     private void Awake()
     {
@@ -23,30 +24,42 @@ public class ControlCarSphere : MonoBehaviour
         baseDragValue = sphereRB.drag;
     }
     private void Update()
-    {
-        float horzMov = Input.GetAxis("Horizontal");
-        float vertcMov = Input.GetAxis("Vertical");
-        if (onGround)
-        {
-            if (horzMov != 0 && vertcMov != 0) transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, horzMov * turningStrength * Time.deltaTime, 0f));
-            if (vertcMov > 0 ) currentMovment = fowardAccleration * vertcMov;
-            else if (vertcMov < 0) currentMovment = backAcceleration * vertcMov;
-            RotateWheels(horzMov);
-        }
+    {        
+        InputCheck();        
         transform.position = sphereRB.position;//keeps the model with the sphere
     }
     private void FixedUpdate()
     {
         onGround = false;
-        RaycastHit hit;
-        if (Physics.Raycast(rayCastPoint.position, -transform.up, out hit, rayCastDistance, groundLayer))
+        RotateCar();
+        MovmentCar();
+
+    }
+    private void RotateWheels(float horzMov)
+    { 
+        foreach(Transform wheel in turningWheels)
+        {
+            wheel.rotation = Quaternion.Euler(0, horzMov * turningStrength, 0);
+        }
+    }
+    void RotateCar()
+    {
+        RaycastHit currentSurface;
+        //if(Physics.Raycast(transform.position, -transform.up, out currentSurface, sphereCollider.radius + .5f)) if wants to make the car not tur back
+        if (Physics.Raycast(transform.position, Vector3.down, out currentSurface, sphereCollider.radius + .5f))
         {
             onGround = true;
+            Quaternion rot = Quaternion.FromToRotation(transform.up, currentSurface.normal) * transform.rotation;
+            if (rot.x < -maxXVehicleTurn || rot.x > maxXVehicleTurn) rot.x = maxXVehicleTurn * Mathf.Sign(rot.x);
+            if (rot.z < -maxZVehicleTurn || rot.x > maxZVehicleTurn) rot.z = maxZVehicleTurn * Mathf.Sign(rot.z);
+            transform.rotation = rot;//rotates the GameObject related to the current surface
         }
+    }
+    void MovmentCar()
+    {
         if (onGround)
         {
             sphereRB.drag = baseDragValue;
-            RotateCar(hit);
             if (Mathf.Abs(currentMovment) > 0) sphereRB.AddForce(transform.forward * currentMovment);
         }
         else
@@ -55,15 +68,16 @@ public class ControlCarSphere : MonoBehaviour
             sphereRB.AddForce(Vector3.up * -gravityForce);//pulls the car back to ground
         }
     }
-    private void RotateWheels(float horzMov)
-    { 
-        foreach(Transform wheel in turningWheels)
-        {
-            wheel.rotation = Quaternion.Euler(wheel.localRotation.eulerAngles.x, horzMov * 30, wheel.localRotation.eulerAngles.z);
-        }
-    }
-    void RotateCar(RaycastHit currentSurface)
+    void InputCheck()
     {
-        transform.rotation = Quaternion.FromToRotation(transform.up, currentSurface.normal) * transform.rotation;//rotates the GameObject related to the current surface
+        float horzMov = Input.GetAxis("Horizontal");
+        float vertcMov = Input.GetAxis("Vertical");
+        if (onGround)
+        {
+            if (horzMov != 0 && vertcMov != 0) transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, horzMov * turningStrength * Time.deltaTime, 0f));
+            if (vertcMov > 0) currentMovment = fowardAccleration * vertcMov;
+            else if (vertcMov < 0) currentMovment = backAcceleration * vertcMov;
+            RotateWheels(horzMov);
+        }
     }
 }
