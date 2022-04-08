39b8d033329767d5e2bc1c00abd44baa0fd3aa76
Bakamura <ra00297288@pucsp.edu.br>
2022-04-07 23:08:01 -0300

    Changes to the map and player
    jump pad script altered to boost forward

--- Assets/TestScene/Scripts/props/JumpPad.cs
+++ Assets/TestScene/Scripts/props/JumpPad.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpPad : MonoBehaviour
-{
+public class JumpPad : MonoBehaviour {
     [SerializeField] private float JumpForce;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            other.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce, ForceMode.VelocityChange);
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "Player") {
+            other.GetComponent<Rigidbody>().velocity = new Vector3(0, JumpForce, JumpForce / 4);
         }
     }
 }
